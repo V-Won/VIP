539383b469215ba9f16b2ba6cf39dd8ac2249e96
VW <89129570+V-Won@users.noreply.github.com>
2024-06-25 21:19:10 +0900

    .

--- happecharger/static/js/front.js
+++ happecharger/static/js/front.js
@@ -40,12 +40,47 @@ document.addEventListener("DOMContentLoaded", function () {
         pinType: scrollContainer.style.transform ? "transform" : "fixed"
       });
 
-      ScrollTrigger.addEventListener("refresh", () => locoScroll.update());
-      ScrollTrigger.refresh();
+      setTimeout(() => {
+        ScrollTrigger.addEventListener("refresh", () => locoScroll.update());
+        ScrollTrigger.refresh();
+      }, 1000);
 
       // GSAP과 ScrollTrigger 플러그인을 로드합니다.
       gsap.registerPlugin(ScrollTrigger);
 
+      // box-header scroll
+      let lastScrollY = 0;
+
+      ScrollTrigger.create({
+        start: 'top -100',
+        end: 99999,
+        scroller: scrollContainer, // Locomotive Scroll과 연동
+        onToggle: self => {
+          const boxHeader = document.querySelector('.box-header');
+          if (self.isActive) {
+            boxHeader.classList.add('is-scroll');
+          } else {
+            boxHeader.classList.remove('is-scroll');
+          }
+        },
+        onUpdate: self => {
+          const boxHeader = document.querySelector('.box-header');
+          const currentScrollY = self.scroll();
+
+          if (currentScrollY > lastScrollY) {
+            boxHeader.classList.add('is-down');
+            boxHeader.classList.remove('is-up');
+          } else {
+            boxHeader.classList.add('is-up');
+            boxHeader.classList.remove('is-down');
+          }
+
+          lastScrollY = currentScrollY;
+        }
+      });
+
+
+      // box-kv
       gsap.to(".box-kv .inner", {
         yPercent: 50, // y축으로 50% 이동
         opacity: 0.2, // 투명도
@@ -58,7 +93,143 @@ document.addEventListener("DOMContentLoaded", function () {
         }
       });
 
-      document.querySelector('.box-header').classList.add('show');
+      // box-banner-bottom
+      gsap.fromTo(".box-banner-bottom .banner_right",
+        {
+          y: 20
+        },
+        {
+          y: -20,
+          scrollTrigger: {
+            trigger: ".box-banner-bottom",
+            scroller: scrollContainer, // Locomotive Scroll과 연동
+            start: "top center",
+            end: "bottom center",
+            scrub: true,
+            markers: false
+          }
+        });
+
+      // box-banner-roller
+      const inner = document.querySelector('.box-banner-roller .inner');
+      const images = inner.querySelectorAll('.box-banner-roller .inner img');
+      const totalWidth = Array.from(images).reduce((acc, img) => acc + img.offsetWidth + 96, 0);
+
+      gsap.set(inner, { x: 0 });
+
+      function animateBanner() {
+        gsap.to(inner, {
+          x: -totalWidth / 2,
+          duration: 25,
+          ease: 'linear',
+          repeat: -1,
+          modifiers: {
+            x: gsap.utils.unitize(x => parseFloat(x) % (totalWidth / 2))
+          }
+        });
+      }
+
+      animateBanner();
+
+      // box-motion
+      const sections = document.querySelectorAll('.section-motion');
+      let index = 0;
+
+      function animateCount(element, finalValue) {
+        gsap.to(element, {
+          innerText: finalValue,
+          duration: 2,
+          ease: "power1.inOut",
+          snap: { innerText: 1 },
+          onUpdate: function () {
+            element.innerText = Math.ceil(this.targets()[0].innerText).toLocaleString() + ' +';
+          }
+        });
+      }
+
+      function activateSection() {
+        sections.forEach((section, i) => {
+          if (i === index) {
+            section.classList.add('is-active');
+            const tt1 = section.querySelector('.tt-1');
+            const finalValue = parseInt(tt1.textContent.replace(/[^0-9]/g, ''));
+
+            animateCount(tt1, finalValue);
+          } else {
+            section.classList.remove('is-active');
+          }
+        });
+
+        index = (index + 1) % sections.length;
+      }
+
+      activateSection();
+      setInterval(activateSection, 5000);
+
+      // count
+      const counters = document.querySelectorAll('.count');
+
+      counters.forEach(counter => {
+        ScrollTrigger.create({
+          trigger: counter,
+          scroller: scrollContainer, // Locomotive Scroll과 연동
+          start: 'top center',
+          onEnter: function () {
+            const finalValue = +counter.getAttribute('data-count');
+
+            gsap.to(counter, {
+              innerText: finalValue,
+              duration: 2,
+              ease: "power1.inOut",
+              snap: { innerText: 1 },
+              onUpdate: function () {
+                counter.innerText = Math.ceil(counter.innerText).toLocaleString() + ' +';
+              }
+            });
+          },
+          once: true // Trigger only once
+        });
+      });
+
+      // box-page is-active
+      const sectionsItem = document.querySelectorAll('.box-section-con .section');
+      const navLinks = document.querySelectorAll('.box-page a');
+
+      sectionsItem.forEach((section, index) => {
+        ScrollTrigger.create({
+          trigger: section,
+          start: "top center",
+          end: "bottom center",
+          scroller: scrollContainer, // Locomotive Scroll과 연동
+          onEnter: () => setActiveLink(index),
+          onEnterBack: () => setActiveLink(index)
+        });
+      });
+
+      function setActiveLink(index) {
+        navLinks.forEach(link => link.classList.remove('is-active'));
+        if (navLinks[index]) {
+          navLinks[index].classList.add('is-active');
+        }
+      }
+
+      ScrollTrigger.create({
+        trigger: ".box-page",
+        start: "top 190px",
+        endTrigger: ".sec-5",
+        end: "bottom bottom",
+        scroller: scrollContainer, // Locomotive Scroll과 연동
+        pin: true,
+        pinSpacing: false
+      });
+
+
+
+
+
+
+
+
     })
     .catch(error => {
       console.error('Error fetching the header or footer:', error);
